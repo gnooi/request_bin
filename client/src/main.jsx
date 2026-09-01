6a75e9d27ae925f04387076d9053182f0b09cccc
Kiya Shahidi <kiyashahidi9@gmail.com>
2026-08-31 20:43:38 -0400

    implemented services and token auth

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,6 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { bootstrapToken } from './auth/auth.js'
+
+await bootstrapToken()
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
